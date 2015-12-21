db25ab0d7d8fe9dea909c614d077d26b360bfef4
karedianoorsil <karedianoorsil@live.com>
2015-12-21 12:26:55 -0500

    fixed values for checkbox and radio

--- Js/content.js
+++ Js/content.js
@@ -1,15 +1,30 @@
 /// <reference path="../Typings/chrome.d.ts" />
 /// <reference path="../Typings/jquery/jquery.d.ts" />
 
-chrome.runtime.onMessage.addListener(function(request,sender,sr){
-	if(request.message == "read"){
-		var input = $(":input");
-		console.log(input);
-		var msg = [];
-		for(var i=0;i<input.length;i++){
-            $(input[i]).attr("value",$(input[i]).val());
-            			console.log(input[i]);
-		}
-		//console.log(msg.toString());
-	}
+chrome.runtime.onMessage.addListener(function (request, sender, sr) {
+    if (request.message == "read") {
+        var input = $(":input");
+        console.log(input);
+        var msg = [];
+        for (var i = 0; i < input.length; i++) {
+            if (isToggleInput(input[i])) {
+                if ($(input[i])[0].checked) {
+                    $(input[i]).attr("checked", "checked");
+                }
+                else {
+                    $(input[i]).removeAttr("checked");
+                    $(input[i])[0].checked = false;
+                }
+            }
+            else {
+                $(input[i]).attr("value", $(input[i]).val());
+                console.log(input[i]);
+            }
+        }
+        //console.log(msg.toString());
+    }
 });
+function isToggleInput(i) {
+    console.log(i.type == "checkbox" || i.type == "radio" ? true : false);
+    return i.type == "checkbox" || i.type == "radio" ? true : false;
+}
