fbbad18d7835ec8f17fde676de61faeb1261a740
karedianoorsil <karedianoorsil@live.com>
2015-12-23 19:55:23 -0500

    query selector updated

--- Js/content.js
+++ Js/content.js
@@ -1,9 +1,9 @@
 /// <reference path="../Typings/chrome.d.ts" />
 /// <reference path="../Typings/jquery/jquery.d.ts" />
-
+var queryString = ":input:visible[type='text'],:input:visible[type='number'],:input:visible[type='checkbox'],:input:visible[type='radio'],:input:visible[type='date'],:input:visible[type='color'],:input:visible[type='range'],:input:visible[type='month'],:input:visible[type='week'],:input:visible[type='time'],:input:visible[type='datetime'],:input:visible[type='datetime-local'],:input:visible[type='email'],:input:visible[type='search'],:input:visible[type='tel'],:input:visible[type='url']";
 chrome.runtime.onMessage.addListener(function (request, sender, sr) {
     if (request.message == "read") {
-        var input = $(":input");
+        var input = $(queryString);
         console.log(input);
         var msg = [];
         for (var i = 0; i < input.length; i++) {
