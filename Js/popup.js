28758589615db332fa0c7a238d11334f5346cbcc
Noorsil Karedia <Noorsil Karedia>
2015-12-25 22:48:05 -0500

    tootip color added

--- Js/popup.js
+++ Js/popup.js
@@ -6,7 +6,7 @@
 
 $(document).ready(function(){
    var port = chrome.runtime.connect({name: "readPort"});
-
+   var that = $(".read-container");
 
 document.getElementById('read-button').addEventListener("click",function(){
 	port.postMessage("read");
@@ -15,17 +15,31 @@ document.getElementById('read-button').addEventListener("click",function(){
 port.onMessage.addListener(function(msg){
     if(msg != "false"){
         $(".data-display").html(msg);
+        $(that).addClass("orange");
+        $(that).attr("data-original-title", "Give a name to your form data");
+        that.tooltip({trigger: 'manual'}).tooltip('enable').tooltip('show');
     }else{
-        var that = $(".read-container");
-        that.tooltip('show');
+
+        $(that).addClass("red");
+        $(that).attr("data-original-title", "No data found on this page");
+
+        that.tooltip({trigger: 'manual'}).tooltip('enable').tooltip('show');
         setTimeout(function() {
-            that.tooltip('hide');
-        }, 3000);
+            that.tooltip({trigger: 'manual'}).tooltip('disable').tooltip('hide');
+            $(that).removeClass("red");
+            $(that).attr("data-original-title", "");
+        }, 3000)
     }
 	console.log(msg);
 }); 
 
-
-
+/*
+$(".read-container").mouseleave(function(){
+    if(tooltipShow){
+        var that = $(".read-container");
+        that.tooltip('show');
+    }
+});
+*/
 
 });
