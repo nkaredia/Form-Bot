5f7bf4b7ceb509fdd9975bcc6b8257610b1dfa98
Noorsil Karedia <karedianoorsil@gmail.com>
2015-12-30 00:40:00 -0500

    set error

--- Js/popup.js
+++ Js/popup.js
@@ -4,68 +4,75 @@
 
 
 
-$(document).ready(function(){
-   var port = chrome.runtime.connect({name: "readPort"});
-   
-   var hasData = false;
+$(document).ready(function () {
+    var port = chrome.runtime.connect({ name: "readPort" });
+
+    var hasData = false;
 
 
 
 
-document.getElementById('read-button').addEventListener("click",function(){
-	port.postMessage("read");
-});
+    document.getElementById('read-button').addEventListener("click", function () {
+        port.postMessage("read");
+    });
 
-port.onMessage.addListener(function(msg){
-    var that = $(".read-container");
-    if(msg == "false"){       
-        $(that).addClass("red");
-        $(that).attr("data-original-title", "No data found on this page");
-
-        that.tooltip({trigger: 'manual'}).tooltip('enable').tooltip('show');
-        setTimeout(function() {
-            that.tooltip({trigger: 'manual'}).tooltip('disable').tooltip('hide');
-            $(that).removeClass("red");
-            $(that).attr("data-original-title", "");
-        }, 3000);
-    }else if("string" == typeof(msg)){
-        $(".data-display").html(msg);
-        $(that).addClass("orange");
-        $(that).attr("data-original-title", "Give a name to your form data");
-        that.tooltip({trigger: 'manual'}).tooltip('enable').tooltip('show');
-        $("#data-name").attr("placeholder", "Name of your data");
-        $("#data-name").val("");
-        hasData = true;
-    }
-	console.log(msg);
-}); 
-
-$(".save,.discard").on('click',function(event){
-    var that = $(this);    
-    if(!hasData){
-        $(that).addClass("red");
-        $(that).attr("data-original-title", "No data to "+$(this).html());
-
-   
-        that.tooltip({trigger: 'manual'}).tooltip('enable').tooltip('show');
-        setTimeout(function() {
-            that.tooltip({trigger: 'manual'}).tooltip('disable').tooltip('hide');
-            $(that).removeClass("red");
-            $(that).attr("data-original-title", "");
-            $(this).blur();
-        }, 3000);
-    }
-
-});
-
-
-/*
-$(".read-container").mouseleave(function(){
-    if(tooltipShow){
+    port.onMessage.addListener(function (msg) {
         var that = $(".read-container");
-        that.tooltip('show');
-    }
-});
-*/
+        if (msg == "false") {
+
+            setError($(".read-container"), "No data found on this page", 3000);
+
+        } else if ("string" == typeof (msg)) {
+            $(".data-display").html(msg);
+            $(that).addClass("orange");
+            $(that).attr("data-original-title", "Give a name to your form data");
+            that.tooltip({ trigger: 'manual' }).tooltip('enable').tooltip('show');
+            $("#data-name").attr("placeholder", "Name of your data");
+            $("#data-name").val("");
+            hasData = true;
+        }
+        console.log(msg);
+    });
+
+    $(".save,.discard").on('click', function (event) {
+        var that = $(this);
+        if (!hasData) {
+            setError(that, "No data to " + $(this).html(), 3000);
+        }
+        else if (hasData) {
+            if (that.hasClass("save")) {
+                if ($("#data-name").val() === "") {
+                    if ($(".read-container").attr("data-original-title") === "") {
+                        setError($(".read-container"), "Please name your data before saving", 3000);
+                    }
+                    else {
+                        $(".read-container").removeClass("orange");
+                        setError($(".read-container"), "Please name your data before saving", 3000);
+                    }
+                }else{
+                    
+                }
+            }
+            else if (that.hasClass("discard")) {
+                if ($("#data-name").val() === "") {
+                    
+                }
+            }
+        }
+    });
 
 });
+
+function setError(element, errMessage, errDuration) {
+    $(element).addClass("red");
+    $(element).attr("data-original-title", errMessage);
+
+
+    element.tooltip({ trigger: 'manual' }).tooltip('enable').tooltip('show');
+    setTimeout(function () {
+        element.tooltip({ trigger: 'manual' }).tooltip('disable').tooltip('hide');
+        $(element).removeClass("red");
+        $(element).attr("data-original-title", "");
+        $(element).blur();
+    }, errDuration);
+}
